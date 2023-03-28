ba5c5ed3276e61955888525904a4901680e110d0
Code Sandbox <hello@codesandbox.io>
2023-03-28 05:50:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d01fdc6b7729d836bbbd8e0a58b410c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce8d69405db3840d3e6d9d4eaf6f23a",
+    "url": "https://akhil3000.github.io/csb-1wnhs5/index.html"
+  },
+  {
+    "revision": "1ad93b262cf519fd96ce",
+    "url": "https://akhil3000.github.io/csb-1wnhs5/static/js/2.a261328b.chunk.js"
+  },
+  {
+    "revision": "c9691f751e6c9c6bd3f9",
+    "url": "https://akhil3000.github.io/csb-1wnhs5/static/js/main.74545c2a.chunk.js"
+  },
+  {
+    "revision": "86e687a3b69b4e0e777d",
+    "url": "https://akhil3000.github.io/csb-1wnhs5/static/js/runtime-main.b84b0e14.js"
+  }
+]);
---
+++ static/js/main.74545c2a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpevent-handling-in-react"]=this["webpackJsonpevent-handling-in-react"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),l=n(2);var c=function(){return r.a.createElement("div",{className:"container"},r.a.createElement("h1",null,"Hello"),r.a.createElement("input",{type:"text",placeholder:"What's your name?"}),r.a.createElement("button",null,"Submit"))};n.n(l).a.render(r.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.74545c2a.chunk.js.map
